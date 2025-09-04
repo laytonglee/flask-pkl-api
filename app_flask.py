0249c6bae84164b0778832c4e6bb21328bf6d935
laytonglee <laytongly@gmail.com>
2025-09-04 15:17:32 +0700

    Testing API

--- app_flask.py
+++ app_flask.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 import os
+
 
 # Load model
 with open("linear_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
